e277ba3a193b507702f42dfd7e72d9949c84d4f9
Musabii Mambetov <musabi@live.com>
2022-06-21 20:54:06 +0300

    add devtools

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todosReducer from "./todosReducer";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-export default configureStore({
-  reducer: { todos: todosReducer },
-});
+export default configureStore(
+  {
+    reducer: { todos: todosReducer },
+  },
+  composeWithDevTools()
+);
